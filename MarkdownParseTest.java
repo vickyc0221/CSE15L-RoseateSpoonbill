4dc16f4f642b3904a413933482cac8edbedbb68e
vickyc0221 <vcaldero@ucsd.edu>
2022-02-24 18:16:20 -0800

    A

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -131,4 +131,40 @@ public class MarkdownParseTest {
         expected.add("https://ucsd-cse15l-w22.github.io/");
         assertEquals(expected, links);
     }
+
+    @Test
+    public void test1() throws IOException{
+        Path fileName = Path.of("LRtest1.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("%60google.com");
+        expected.add("google.com");
+        expected.add("ucsd.edu");
+        assertEquals(expected, links);
+    }
+
+    @Test
+    public void test2() throws IOException{
+        Path fileName = Path.of("LRtest2.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("a.com");
+        expected.add("a.com(())");
+        expected.add("example.com");
+        assertEquals(expected, links);
+    }
+
+    @Test
+    public void test3() throws IOException{
+        Path fileName = Path.of("LRtest3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://ucsd-cse15l-w22.github.io/");
+        assertEquals(expected, links);
+    }
+
+
 }
